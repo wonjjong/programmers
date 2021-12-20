aa35598c3518e598b917ba1399e205d32aa40067
wonjjong <wonjjong.dev@gmail.com>
2021-12-20 21:20:56 +0900

    level2 N^2배열 자르기

---
+++ src/main/java/level2/N배열자르기/Solution.java
@@ -0,0 +1,38 @@
+package level2.N배열자르기;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public static void main(String[] args) {
+        int n = 3; int left = 2 ; int right = 5;
+        int[] solution = solution(n, left, right);
+
+        for (int i : solution) {
+            System.out.print(i + " ");
+        }
+        System.out.println("solution = " + solution);
+    }
+
+    public static int[] solution(int n, long left, long right) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        int startIndex = (int)(left / n);
+        int endIndex = (int)(right / n);
+        int[] answer = new int[(int)(right-left+1)];
+
+        for (int i = startIndex; i <= endIndex; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                arrayList.add(i + 1);
+            }
+            for (int j = i + 2; j <= n; j++)
+                arrayList.add(j);
+        }
+
+        int index =0;
+        for(int i= (int)(left % n); i<= (right-left) + (int)(left %n); i++) {
+            answer[index] = arrayList.get(i);
+            index++;
+        }
+
+        return answer;
+    }
+}
